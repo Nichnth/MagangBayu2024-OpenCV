e5d902f3fbd9a9f433c0a838e94430d03d13501b
nichnth <nicholasaabel@gmail.com>
2024-01-12 16:20:37 +0700

    tugas 3 done

--- tugas/tugas3/tugas3.py
+++ tugas/tugas3/tugas3.py
@@ -1,4 +1,19 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-# Tulis Kodingan kalian dibawah
+lower = np.array([0, 50, 50])
+upper = np.array([40, 170, 170])
+
+vid = cv.VideoCapture(0)
+
+while True:
+    succes, img = vid.read()
+    image= cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(image, lower, upper)
+    mask_m = cv.flip(mask, 1)
+
+    cv.imshow("Camera", mask_m)
+    if cv.waitKey(1) == 27: break
+
+vid.release()
+cv.destroyAllWindows()
